f472b972b0174ff66d4e041b5bbde274810c9e7a
Turețchi Gabriel <g.turetchi2001@gmail.com>
2024-12-08 19:58:05 +0200

    Animtaion on scroll

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import type { MetaFunction } from "@remix-run/node";
-import { motion, AnimatePresence } from "framer-motion";
+
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,57 +15,54 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const [isVisible, setIsVisible] = useState(false); // Visibility state
+  const svgRef = useRef(null);
+  const [pathLength, setPathLength] = useState(0);
+  const { scrollYProgress } = useScroll();
+
+  const strokeDashoffset = useTransform(
+    scrollYProgress,
+    [0, 1],
+    [pathLength, 0]
+  );
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsVisible(true);
-    }, 2600);
-
-    return () => clearTimeout(timer);
+    const path = svgRef.current as SVGPathElement | null;
+    if (path) {
+      const length = path.getTotalLength();
+      setPathLength(length);
+    }
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center bg-white space-y-0 snap-y">
-      <div className="h-screen w-screen flex flex-col items-center justify-center pl-5 pr-5 bg-tertiary snap-center">
-        <motion.img
-          src="gif/animated-logo.gif"
-          alt="Muntenia Logo"
-          className="w-[75vw] justify-self-end"
-          initial={{ scale: 1 }}
-          animate={{ scale: 0.6, y: [0, -200, 0] }}
-          transition={{
-            type: "spring",
-            stiffness: 80,
-            duration: 0.8,
-            times: [0, 0.5, 1],
+    <>
+      <motion.svg
+        viewBox="0 0 979 3841"
+        fill="none"
+        preserveAspectRatio="xMidYMax meet"
+        height="200vh"
+      >
+        <motion.path
+          ref={svgRef}
+          id="line-path"
+          d="M615.5 0.5C615.5 0.5 735 2009 615.5 2185C496 2361 376.5 2350.5 380 2420.5C383.5 2490.5 425 2594.5 313.5 2584.5C202 2574.5 313.5 2631.5 313.5 2683C313.5 2734.5 418 2727.5 399 2784.5C380 2841.5 532.5 2794.5 583.5 2784.5C634.5 2774.5 896 2748.5 830 2690C764 2631.5 995.5 2474.5 910 2490.5C824.5 2506.5 807.5 2356 783 2396C758.5 2436 583.5 2440.5 634.5 2459C685.5 2477.5 539 2664.5 599 2666C659 2667.5 709 2951.5 643 2968.5C577 2985.5 698.5 3106.5 783 3081C867.5 3055.5 919.5 3199.5 937 3232.5C954.5 3265.5 1003 3311 937 3329.5C871 3348 916 3498 867.5 3489.5C819 3481 767.5 3668 653 3678C538.5 3688 604.5 3747.5 546 3765C487.5 3782.5 477.5 3829 477.5 3829C477.5 3829 219.5 3692 211 3622C202.5 3552 168 3489.5 73.9999 3482.5C-20.0001 3475.5 9.49991 3371 24.9999 3311C40.4999 3251 142.5 3209.5 90.9999 3174C39.4999 3138.5 222 3018.5 262 3125.5C302 3232.5 383.5 3174 383.5 3174L463.5 3248L493.5 3311L527.5 3248L589 3185.5L690.5 3174L867.5 3209.5"
+          strokeWidth="15"
+          stroke="red"
+          strokeDasharray={pathLength}
+          style={{
+            strokeDashoffset,
           }}
-          layout
         />
-        <AnimatePresence mode="popLayout">
-          {isVisible && (
-            <motion.h1
-              className="text-xxxl font-extrabold text-primary m-0 p-0"
-              initial={{ opacity: 0, scale: 0 }}
-              animate={{ opacity: 1, y: 0, scale: 1 }}
-              exit={{ opacity: 0, scale: 0 }}
-              transition={{
-                type: "spring",
-                stiffness: 100,
-                damping: 15,
-                duration: 0.8,
-              }}
-            >
-              Muntenia<sup className="text-lg align-super">©</sup>
-            </motion.h1>
-          )}
-        </AnimatePresence>
-      </div>
-      <div className="h-screen bg-secondary w-screen snap-center">
-        <p className="text-tertiary">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
-      </div>
-    </div>
+      </motion.svg>
+      <p>Path Length: {pathLength}</p>
+    </>
   );
 }
+
+//
+// WEB PAGE DESIGN
+// 1. Logo + Title
+// 2. About Us
+// 3. Services
+// 4. Contact Us
+// 5. Reviews
+//
