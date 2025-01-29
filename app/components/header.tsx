b29f54f84dc9efd2bb6ae42421413f5db3f3b6d2
Turețchi Gabriel <g.turetchi2001@gmail.com>
2025-01-29 23:30:50 +0200

    Header styling

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,12 @@
 export default function Header() {
   return (
     <>
-      <div className="w-screen h-screen flex align-middle justify-center bg-orange-800">
-        Header
+      <div className="w-screen h-screen flex justify-start bg-secondary overflow-hidden">
+        <div className="flex flex-col justify-center items-start w-screen p-0">
+          <div className="h-screen text-[13rem] font-extrabold text-white [writing-mode:vertical-lr] leading-none absolute left-0 translate-x-[-15%]">
+            Muntenia
+          </div>
+        </div>
       </div>
     </>
   );
